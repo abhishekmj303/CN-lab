90035781f3cfb4744c58714dd50810ac60346e5a
Abhishek M J <abhishekmj303@gmail.com>
2023-08-24 20:53:35 +0530

    Update lab05 - 2 way file transfer

--- 05-Lab/client.py
+++ 05-Lab/client.py
@@ -1,7 +1,9 @@
 import socket
 import threading
 import os
+import time
 import readline
+from urllib.parse import quote, unquote
 
 IP = socket.gethostbyname(socket.gethostname())
 # IP = "172.16.19.141"
@@ -15,6 +17,12 @@ DISCONNECT_MESSAGE = "QUIT!"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connected = True
+
+# MSG Format:
+#   MSG/<msg>
+#   CREATE/<file_name>
+#   POST/<file_name>/<file_content>
+#   GET/<file_name>
 
 folder_path = "client/"
 
@@ -37,6 +45,48 @@ def input_msg(string):
 
     return result
 
+
+def send_msg(conn, msg):
+    conn.send(f"MSG/{quote(msg)}".encode(FORMAT))
+
+
+def handle_file_request(request, body):
+    if request == "CREATE":
+        file_name = unquote(body)
+        file_path = folder_path + file_name
+        print_msg(f"[SERVER] Create file '{file_name}'")
+        with open(file_path, "w"):
+            msg = f"File name recieved"
+            send_msg(client, msg)
+            time.sleep(0.1)
+            client.send(f"GET/{quote(file_name)}".encode(FORMAT))
+
+    elif request == "POST":
+        file_name, file_content = map(unquote, body.split("/", 1))
+        file_path = folder_path + file_name
+        print_msg(f"[SERVER] Contents of '{file_name}': {file_content}")
+        with open(file_path, "a") as f:
+            f.write(file_content)
+            f.write("\n")
+            msg = f"File contents of '{file_name}' recieved"
+            send_msg(client, msg)
+
+    elif request == "GET":
+        file_name = unquote(body)
+        file_path = folder_path + file_name
+        try:
+            with open(file_path, "r") as f:
+                file_content = f.read()
+                client.send(f"POST/{quote(file_name)}/{quote(file_content)}".encode(FORMAT))
+        except Exception as e:
+            err = f"Error: {e}"
+            print_msg(err)
+            send_msg(client, err)
+
+    else:
+        send_msg(client, f"Error: Invalid request '{request}'.")
+
+
 def handle_server():
     global connected
     while connected:
@@ -44,7 +94,26 @@ def handle_server():
         if not msg or msg == DISCONNECT_MESSAGE:
             connected = False
             break
-        print_msg(f"[SERVER] {msg}")
+        
+        try:
+            request, body = msg.split("/", 1)
+        except Exception as e:
+            err = "Error: Invalid request format."
+            print_msg(err)
+            send_msg(client, err)
+            continue
+
+        if request == "MSG":
+            print_msg(f"[SERVER] {unquote(body)}")
+            continue
+
+        try:
+            handle_file_request(request, body)
+        except Exception as e:
+            err = f"Error: {e}"
+            print_msg(err)
+            send_msg(client, err)
+            continue
 
     client.close()
     print_msg(f"[DISCONNECTED] Server disconnected from Client.")
@@ -60,15 +129,11 @@ def main():
 
     global connected
     while connected:
-        for i in range(2):
-            if i == 0:
-                msg = input_msg("Enter file name (with ext): ")
-            else:
-                with open(folder_path+msg, "r") as f:
-                    msg = f.read()
-            client.send(msg.encode(FORMAT))
-            if msg == DISCONNECT_MESSAGE:
-                break
+        msg = input_msg("(file_name)> ").strip()
+        if msg == DISCONNECT_MESSAGE:
+            break
+
+        client.send(f"CREATE/{quote(msg)}".encode(FORMAT))
 
     client.close()
     print_msg(f"[DISCONNECTED] Client disconnected from {IP}:{PORT}")
--- 05-Lab/server.py
+++ 05-Lab/server.py
@@ -1,6 +1,9 @@
 import socket
 import threading
 import os
+import time
+import readline
+from urllib.parse import quote, unquote
 from dataclasses import dataclass
 
 # IP = socket.gethostbyname(socket.gethostname())
@@ -11,6 +14,12 @@ SIZE = 1024
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "QUIT!"
 
+# MSG Format:
+#   MSG/<msg>
+#   CREATE/<file_name>
+#   POST/<file_name>/<file_content>
+#   GET/<file_name>
+
 folder_path = "server/"
 
 clients = []
@@ -20,43 +29,141 @@ class Client:
     conn: socket.socket
     addr: str
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
 
-    for i in range(2):
-        msg = conn.recv(SIZE).decode(FORMAT)
+def find_client(addr):
+    for client in clients:
+        if client.addr == addr:
+            return client
+    return None
+
+
+current_input = ""
+
+def print_msg(msg):
+    if not current_input:
+        print(msg)
+    else:
+        input_buffer = readline.get_line_buffer()
+        print(f"\r{msg}\n{current_input}{input_buffer}", end="", flush=True)
+
+
+def input_msg(string):
+    global current_input
+
+    current_input = string
+    result = input(f"\r{string}")
+    current_input = ""
+
+    return result
+
+
+def send_msg(conn, msg):
+    conn.send(f"MSG/{quote(msg)}".encode(FORMAT))
+
+
+def server_input():
+    while True:
+        to_addr = input_msg("(ip:port)> ").strip()
+        if to_addr == DISCONNECT_MESSAGE:
+            raise KeyboardInterrupt
+        elif to_addr in ["", "list", "ls"]:
+            print_msg("Active clients:")
+            for client in clients:
+                print_msg(f"  {client.addr}")
+            continue
+
+        to_client = find_client(to_addr)
+        if to_client is None:
+            print_msg(f"Error: Client '{to_addr}' not found.")
+            continue
+
+        msg = input_msg("(file_name)> ")
         if msg == DISCONNECT_MESSAGE:
-            break
+            raise KeyboardInterrupt
+        elif msg == "":
+            continue
 
-        opts = {0: "Create", 1: "File Content"}
-        print(f"[{addr}] {opts[i]}: {msg}")
+        to_client.conn.send(f"CREATE/{quote(msg)}".encode(FORMAT))
+
+
+def handle_file_request(client, request, body):
+    if request == "CREATE":
+        file_name = unquote(body)
+        file_path = folder_path + file_name
+        print_msg(f"[{client.addr}] Create file '{file_name}'")
+        with open(file_path, "w"):
+            msg = f"File name recieved"
+            send_msg(client.conn, msg)
+            time.sleep(0.1)
+            client.conn.send(f"GET/{quote(file_name)}".encode(FORMAT))
+
+    elif request == "POST":
+        file_name, file_content = map(unquote, body.split("/", 1))
+        file_path = folder_path + file_name
+        print_msg(f"[{client.addr}] Contents of '{file_name}': {file_content}")
+        with open(file_path, "a") as f:
+            f.write(file_content)
+            f.write("\n")
+            msg = f"File contents of '{file_name}' recieved"
+            send_msg(client.conn, msg)
+
+    elif request == "GET":
+        file_name = unquote(body)
+        file_path = folder_path + file_name
+        try:
+            with open(file_path, "r") as f:
+                file_content = f.read()
+                client.conn.send(f"POST/{quote(file_name)}/{quote(file_content)}".encode(FORMAT))
+        except Exception as e:
+            err = f"Error: {e}"
+            print_msg(err)
+            send_msg(client.conn, err)
+
+    else:
+        send_msg(client.conn, f"Error: Invalid request '{request}'.")
+
+
+def handle_client(conn, addr):
+    print_msg(f"[NEW CONNECTION] {addr} connected.")
+
+    while True:
+        msg = conn.recv(SIZE).decode(FORMAT)
+        if not msg or msg == DISCONNECT_MESSAGE:
+            break
 
         try:
-            if i == 0:
-                file_name = msg
-                file_path = folder_path + file_name
-                with open(file_path, "w"):
-                    conn.send(f"File name recieved: {msg}".encode(FORMAT))
-            else:
-                with open(file_path, "a") as f:
-                    f.write(msg)
-                    f.write("\n")
-                    conn.send(f"File contents of {file_name} recieved: {msg}".encode(FORMAT))
+            request, body = msg.split("/", 1)
         except Exception as e:
-            print(e)
-            conn.send(f"Error: {e}".encode(FORMAT))
-            break
+            err = "Error: Invalid request format."
+            print_msg(f"[{addr}] {err}")
+            send_msg(conn, err)
+            continue
+
+        if request == "MSG":
+            print_msg(f"[{addr}] {unquote(body)}")
+            continue
+
+        try:
+            handle_file_request(find_client(addr), request, body)
+        except Exception as e:
+            err = f"Error: {e}"
+            print_msg(f"[{addr}] {err}")
+            send_msg(conn, err)
+            continue
 
     conn.close()
     clients.remove(Client(conn, addr))
-    print(f"[DISCONNECTED] {addr} disconnected.")
-    print(f"[ACTIVE CONNECTIONS] {threading.active_count()-2}")
+    print_msg(f"[DISCONNECTED] {addr} disconnected.")
+    print_msg(f"[ACTIVE CONNECTIONS] {threading.active_count()-3}")
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDR)
     server.listen()
-    print(f"[STARTING] Server is listening on {IP}:{PORT}")
+    print_msg(f"[STARTING] Server is listening on {IP}:{PORT}")
+
+    server_input_thread = threading.Thread(target=server_input)
+    server_input_thread.start()
 
     while True:
         conn, addr = server.accept()
@@ -67,16 +174,16 @@ def main():
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
 
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}")
+        print_msg(f"[ACTIVE CONNECTIONS] {threading.active_count()-2}")
 
 if __name__ == "__main__":
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-    print(f"[SERVER] Keep the files to send/recieve inside folder: '{folder_path}'")
+    print_msg(f"[SERVER] Keep the files to send/recieve inside folder: '{folder_path}'")
     try:
         main()
     except KeyboardInterrupt:
-        print("\n[EXITING] Server is shutting down...")
+        print_msg("\n[EXITING] Server is shutting down...")
         for client in clients:
             client.conn.send(DISCONNECT_MESSAGE.encode(FORMAT))
             # client.conn.close()
